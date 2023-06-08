e133a2359d8dab7fce631fd5cb417e3660bd5ccb
catherinefc21 <129704819+catherinefc21@users.noreply.github.com>
2023-06-08 19:15:19 -0400

    inicio google

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,20 @@
-// aqui exportaras las funciones que necesites
+import { signInWithEmailAndPassword,GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import { auth, provider } from '../firebase/firebaseConfig';
 
-export const myFunction = () => {
-  // aqui tu codigo
-  console.log('Hola mundo!');
-};
+export const createUser = (email, contraseña) => {
+  signInWithEmailAndPassword(auth, email, contraseña);
+}
+
+export const loginGoogle = () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+    });
+}
