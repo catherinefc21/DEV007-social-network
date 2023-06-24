0f1eb04fb2eda0abaf44b96ee5427b18a5e501d3
catherinefc21 <129704819+catherinefc21@users.noreply.github.com>
2023-06-24 14:29:01 -0400

    agrega detalles

--- src/lib/index.js
+++ src/lib/index.js
@@ -59,19 +59,18 @@ export const loginGoogle = (onNavigate) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
       // eslint-disable-next-line no-unused-vars
-      const token = credential.accessToken;
+      credential.accessToken;
       // The signed-in user info.
-      const user = result.user;
+      result.user;
     // IdP data available using getAdditionalUserInfo(result)
     // ...
     }).catch((error) => {
-    // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
+      error.code;
+      error.message;
       // The email of the user's account used.
-      const email = error.customData.email;
+      error.customData.email;
       // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
+      GoogleAuthProvider.credentialFromError(error);
     // ...
     });
 };
